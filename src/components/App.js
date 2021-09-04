3223301517e3a0d0a52c04c366d2bc90bd1caa27
Rafał Gondek <rafalgondek@onet.pl>
2021-09-04 13:58:01 +0200

    only sketch

---
+++ src/components/App.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react'
+import './App.css'
+
+class App extends Component {
+  state = {  }
+  render() {
+    return (
+      <div className="App">
+        <div className="container-squares">
+          <div className="square">
+            Add new note
+          </div>
+          <div className="square">
+            Add new note
+          </div>
+          <div className="square">
+            Add new note
+          </div>
+          <div className="square">
+            Add new note
+          </div>
+          <div className="square">
+            Add new note
+          </div>
+          <div className="square">
+            Add new note
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+ 
+export default App;
